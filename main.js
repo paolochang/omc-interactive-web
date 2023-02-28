69e568dcda1ec3a228208dfed58f93ce67f6fff1
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-27 23:33:27 -0500

    add intersection observer

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
 (() => {
   const stepElems = document.querySelectorAll(".step");
   const graphicElems = document.querySelectorAll(".graphic-item");
-  let currentActive;
+  let currentActive = graphicElems[0];
+  let ioIndex;
+
+  const io = new IntersectionObserver((entries, ovserver) => {
+    ioIndex = +entries[0].target.dataset.index;
+  });
 
   for (let i = 0; i < stepElems.length; i++) {
-    // stepElems[i].setAttribute("data-index", i);
+    io.observe(stepElems[i]);
     stepElems[i].dataset.index = i;
-    // graphicElems[i].setAttribute("data-index", i);
     graphicElems[i].dataset.index = i;
+  }
+
+  function activate() {
+    currentActive.classList.add("visible");
+  }
+
+  function inactivate() {
+    currentActive.classList.remove("visible");
   }
 
   window.addEventListener("scroll", () => {
     let step;
     let boundingRect;
-    for (let i = 0; i < stepElems.length; i++) {
+
+    for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
       step = stepElems[i];
+
+      if (!step) continue;
+
       boundingRect = step.getBoundingClientRect();
 
       if (
         boundingRect.top > window.innerHeight * 0.1 &&
         boundingRect.top < window.innerHeight * 0.8
       ) {
-        if (currentActive) {
-          currentActive.classList.remove("visible");
-        }
+        inactivate();
         currentActive = graphicElems[step.dataset.index];
-        currentActive.classList.add("visible");
+        activate();
       }
     }
   });
+
+  activate();
 })();
